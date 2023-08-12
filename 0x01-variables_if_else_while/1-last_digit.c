1dd1b2d346d4c9ee53ef8d7bd6ba73ccbb9101e7
DamiDii <dafolabi71@gmail.com>
2023-08-12 18:14:22 +0100

    Get the last digit from a number and test it through some conditionals

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,11 +22,11 @@ int main(void)
 	/* finding your last digit (modulus)*/
 	digit = n % 10;
 	/* conditional statemnts to display results*/
-	if (n < 6 && n != 0)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
+	if (n == 0)
+		printf("Last digit of %d is %d and is 0\n", n, digit);
 	else if (n > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, digit);
-	else if (n == 0)
-		printf("Last digit of %d is %d and is 0\n", n, digit);
+	else if (n < 6 && n != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
 	return (0);
 }
