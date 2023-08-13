bf582d59f7e48474e90a0dc4388d91d7742774c4
DamiDii <dafolabi71@gmail.com>
2023-08-13 18:08:48 +0100

    Print single digits of base 10 using int

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 /**
   * main -Entry point
-  * Description: Print out the single digits of base 10
-  *Return: Always 0 (Success)
+  * description: Print out the single digits of base 10
+  * Return: Always 0 (Success)
   */
-
 int main(void)
 {
-	char c = '0';
+	int c = '0';
 
 	for (c = '0'; c <= '9'; c++)
 		putchar(c);
