b6ab0da1e975668e8ab54f2aa79ca90af44f212a
DamiDii <dafolabi71@gmail.com>
2023-08-13 15:16:57 +0100

    Print single digits of base 10

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,15 +2,10 @@
 /**
   * main -Entry point
   * Description: Print out the single digits of base 10
-  *Return: Always 0 (Success)
+  * Return: Always 0 (Success)
   */
-
 int main(void)
 {
-	char c = '0';
-
-	for (c = '0'; c <= '9'; c++)
-		putchar(c);
-	putchar('\n');
+	printf("0123456789");
 	return (0);
 }
