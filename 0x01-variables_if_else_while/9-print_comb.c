e47e99ddcc6fe94a558cc7470132f30dfaf3a217
DamiDii <dafolabi71@gmail.com>
2023-08-14 10:01:10 +0100

    Print 1-9 with a comma and space between

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+  * main -Entry point
+  * description: Print out the single digits of base 10
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int c = '0';
+	int d = '9';
+
+	for (c = '0'; c <= d; c++)
+	{
+		if (c < d)
+		{
+			putchar(c);
+			putchar(',');
+			putchar(' ');
+		}
+		else if (c == d)
+			putchar(c);
+	}
+	return (0);
+}
