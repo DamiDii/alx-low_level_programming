8ecf5a572fd0878a2ddac96b03af37767b9430ca
DamiDii <dafolabi71@gmail.com>
2023-08-16 21:35:51 +0100

    Using _putchar fuction created by user to print thealphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  *main -entry point
+  * description using _putchar to print out the alphabet"
+  * Return: always 0 (success)
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122, i++)
+	{
+		_putchar('i');
+	}
+	return (0);
+}
