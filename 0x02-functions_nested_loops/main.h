51e09c40ff724b054724f909a1ddbc0cf2f6d680
DamiDii <dafolabi71@gmail.com>
2023-08-16 21:10:57 +0100

    Header file called main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+  * File: main.h
+  * Description: a header file that will contain all functions created by me and used for 0x02 project.
+ */
+int _putchar(char a);
+
+#endif
