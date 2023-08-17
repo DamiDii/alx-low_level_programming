ec82af5d8aa84bf861c2e2985cac1892e39a4eb2
DamiDii <dafolabi71@gmail.com>
2023-08-17 13:28:42 +0100

    print the alphabet x10 in lowercase using _putchar

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,9 +10,9 @@ void print_alphabet_x10(void)
 
 	while (n <= 10)
 	{
-		for (i = 97; i <= 122; i++)
+		for (i = 'a'; i <= 'z'; i++)
 		{
-			_putchar('i' + 0);
+			_putchar(i);
 		}
 		_putchar('\n');
 		n++;
