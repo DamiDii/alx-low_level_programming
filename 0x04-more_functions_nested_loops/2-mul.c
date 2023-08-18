1ac98d6a63845b3a9be1d0eb2ad4fe8800c40de4
DamiDii <dafolabi71@gmail.com>
2023-08-18 15:11:43 +0100

    Testing the function that multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Checks if a character  is uppercase
+ * @a: is the first integer to be checked
+ * @b: is the second integer ti be checked
+ * Return: result of a x b
+ */
+
+int mul(int a, int b)
+{
+	int n;
+
+	n = a * b;
+	return (n);
+}
