f21d511476ca618d163f8d097422b6b94dedfd03
DamiDii <dafolabi71@gmail.com>
2023-08-23 17:59:12 +0100

    Task uploaded

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
-  * _strlen - is used to calculate the lenght of a string
-  * @s is the pointer to the str we are getting the lenght to
+  * _strlen - used to calculate the lenght of a string
+  * @s: the pointer to the str we are getting the lenght to
   * Return: int (value of string)
   */
 
 int _strlen(char *s)
 {
 	int len = 0;
-	
-	while( *s != '\0' )
+
+	while (*s != '\0')
 	{
 		len++;
 		s++;
